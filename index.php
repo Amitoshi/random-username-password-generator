a261be237af4bc636a6e50cf5f04a55c09c43957
Amitoshi <71155696+Amitoshi@users.noreply.github.com>
2022-09-25 03:01:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,57 +16,7 @@
             <h1>Random User Name </h1>
             <div>
              <h1>Amit</h1?
-    <?php
-for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') - 1;$i != 7;$x = rand(0, $z), $s.= $a[$x], $i++);
-echo '<input type="text" name="txt" class="txt" style="border: none; width: 250px;
-    padding: 5px;" value="' . $s . '"> 
-<input type="button" value="copy" class="btn" >';
-echo "<br>";
-echo "<br>";
-for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') - 1;$i != 7;$x = rand(0, $z), $s.= $a[$x], $i++);
-echo '<input type="text" style="border: none; width: 250px;
-    padding: 5px; " name="txt2" class="txt2" value="' . $s . '"> 
-<input type="button" value="copy" class="btn2">';
-echo "<br>";
-echo "<br>";
-for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') - 1;$i != 7;$x = rand(0, $z), $s.= $a[$x], $i++);
-echo '<input type="text" style="border: none; width: 250px;
-    padding: 5px;" name="txt3" class="txt3" value="' . $s . '"> 
-<input type="button" value="copy" class="btn3">';
-echo "<br>";
-echo "<br>";
-for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') - 1;$i != 7;$x = rand(0, $z), $s.= $a[$x], $i++);
-echo '<input type="text" style="border: none; width: 250px;
-    padding: 5px;" name="txt4" class="txt4" value="' . $s . '"> 
-<input type="button" value="copy" class="btn4">';
-echo "<br>";
-echo "<br>";
-?>
-</center>
-</div>
-  <script> 
-   let btn=document.querySelector('.btn'); 
-      btn.addEventListener("click", function () {
-       let txt=document.querySelector('.txt'); 
-      txt.select(); 
-      document.execCommand ('copy');  });
- </script><script> 
-    let btn2=document.querySelector('.btn2'); 
-      btn2.addEventListener("click", function () {
-       let txt2=document.querySelector('.txt2'); 
-      txt2.select(); 
-      document.execCommand ('copy');  });</script> <script> 
-    let btn3=document.querySelector('.btn3'); 
-      btn3.addEventListener("click", function () {
-       let txt3=document.querySelector('.txt3'); 
-      txt3.select(); 
-      document.execCommand ('copy');  });</script><script> 
-    let btn4=document.querySelector('.btn4'); 
-      btn4.addEventListener("click", function () {
-       let txt4=document.querySelector('.txt4'); 
-      txt4.select(); 
-      document.execCommand ('copy');  });
- </script>
+  
     </body>
 </html>
 
