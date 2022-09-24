e173a63e0d4d9d70099f9c44baf26d92b97ce122
Amitoshi <71155696+Amitoshi@users.noreply.github.com>
2022-09-25 02:04:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,42 @@
-<!-- /* 
-/ Author Name:AmitoshiYT
-*/ -->
-<!DOCTYPE html><html>
-<head><title>AmitoshiYT</title></head><body style="background-color: gray;"><center><h1>Random User Name </h1><div class="padded" >
-    <?php 
-    for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')-1; $i != 7; $x = rand(0,$z), $s .= $a[$x], $i++);
+ <<!DOCTYPE html>
+ <html>
+ <head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="random username password generator" content="HTML, CSS, JavaScript PHP">
+    <meta name="random text generator" >
+    <meta name="text generator" >
+    <meta name="produce random text password name" >
+
+    <title>AmitoshiYT</title>
+</head>
+    <body style="background-color: gray;">
+        <center>
+            <h1>Random User Name </h1>
+            <div>
+    <?php
+for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') - 1;$i != 7;$x = rand(0, $z), $s.= $a[$x], $i++);
 echo '<input type="text" name="txt" class="txt" style="border: none; width: 250px;
-    padding: 5px;" value="'.$s.'"> 
+    padding: 5px;" value="' . $s . '"> 
 <input type="button" value="copy" class="btn" >';
 echo "<br>";
-echo "<br>";for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')-1; $i != 7; $x = rand(0,$z), $s .= $a[$x], $i++);
+echo "<br>";
+for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') - 1;$i != 7;$x = rand(0, $z), $s.= $a[$x], $i++);
 echo '<input type="text" style="border: none; width: 250px;
-    padding: 5px; " name="txt2" class="txt2" value="'.$s.'"> 
+    padding: 5px; " name="txt2" class="txt2" value="' . $s . '"> 
 <input type="button" value="copy" class="btn2">';
 echo "<br>";
-echo "<br>";for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')-1; $i != 7; $x = rand(0,$z), $s .= $a[$x], $i++);
+echo "<br>";
+for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') - 1;$i != 7;$x = rand(0, $z), $s.= $a[$x], $i++);
 echo '<input type="text" style="border: none; width: 250px;
-    padding: 5px;" name="txt3" class="txt3" value="'.$s.'"> 
+    padding: 5px;" name="txt3" class="txt3" value="' . $s . '"> 
 <input type="button" value="copy" class="btn3">';
 echo "<br>";
-echo "<br>";for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')-1; $i != 7; $x = rand(0,$z), $s .= $a[$x], $i++);
+echo "<br>";
+for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') - 1;$i != 7;$x = rand(0, $z), $s.= $a[$x], $i++);
 echo '<input type="text" style="border: none; width: 250px;
-    padding: 5px;" name="txt4" class="txt4" value="'.$s.'"> 
+    padding: 5px;" name="txt4" class="txt4" value="' . $s . '"> 
 <input type="button" value="copy" class="btn4">';
 echo "<br>";
 echo "<br>";
@@ -50,9 +65,11 @@ echo "<br>";
        let txt4=document.querySelector('.txt4'); 
       txt4.select(); 
       document.execCommand ('copy');  });
- </script></body></html>
+ </script>
+    </body>
+</html>
 
 
- 
+
 
 
