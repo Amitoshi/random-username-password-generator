e13ccb1cd9138153100b099e0e94bc9ee82ad7d8
Amitoshi <71155696+Amitoshi@users.noreply.github.com>
2022-09-25 03:05:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,20 @@
             <h1>Random User Name </h1>
             <div>
              <h1>Amit</h1?
+              <script>
+              function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+console.log(makeid(5));
+                                </script>
   
     </body>
 </html>
